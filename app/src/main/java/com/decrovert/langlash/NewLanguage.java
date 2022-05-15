b70638121f98b2549aed61f964f46f2c38eb9459
decrovert <decrovertp@gmail.com>
2022-05-15 17:04:00 +0100

    adding an activity for creating languages

--- app/src/main/java/com/decrovert/langlash/NewLanguage.java
+++ app/src/main/java/com/decrovert/langlash/NewLanguage.java
@@ -2,12 +2,38 @@ package com.decrovert.langlash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewLanguage extends AppCompatActivity {
+    private Button confirm_button, cancel_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_language);
+
+        confirm_button = findViewById(R.id.new_language_confirm_button);
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNewLanguage();
+                finish();
+            }
+        });
+
+        cancel_button = findViewById(R.id.new_language_cancel_button);
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void saveNewLanguage() {
+        // TODO
     }
 }
