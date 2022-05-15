26f3cc9de80a8e934921cd029edff3c8b6c2acd9
decrovert <decrovertp@gmail.com>
2022-05-15 16:20:20 +0100

    adding button to create new languages

---
+++ app/src/main/java/com/decrovert/langlash/NewLanguage.java
@@ -0,0 +1,13 @@
+package com.decrovert.langlash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewLanguage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_language);
+    }
+}
